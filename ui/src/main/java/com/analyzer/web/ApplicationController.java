f7132309fb0545ab8f035eaf95376ccc23145843
Rachel Charow <rachel.charow@gmail.com>
2016-10-01 14:28:36 -0400

    Update app controller based on changes to JiraService

--- ui/src/main/java/com/analyzer/web/ApplicationController.java
+++ ui/src/main/java/com/analyzer/web/ApplicationController.java
@@ -1,22 +1,31 @@
 package com.analyzer.web;
 
+import com.analyzer.domain.JiraProject;
+import com.analyzer.service.JiraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 /**
  * Created by rcharow on 9/21/16.
  */
+@Component
 @RestController
+@RequestMapping("/api")
 public class ApplicationController {
-    @RequestMapping("/resource")
-    public Map<String,Object> home() {
-        Map<String,Object> model = new HashMap<String,Object>();
-        model.put("test", "Hello New World");
-        return model;
+
+    @Autowired
+    private JiraService jiraService;
+
+    @RequestMapping("/projects")
+    public List<JiraProject> getProjects() {
+        return jiraService.getProjects();
     }
 }
