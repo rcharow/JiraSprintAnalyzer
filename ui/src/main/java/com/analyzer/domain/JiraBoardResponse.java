3457a0492f462c2f14bfd329e54584c9975d4dcd
Rachel Charow <rachel.charow@gmail.com>
2016-10-08 17:15:10 -0400

    Change JiraBoardResponse to extend JiraResponse

--- ui/src/main/java/com/analyzer/domain/JiraBoardResponse.java
+++ ui/src/main/java/com/analyzer/domain/JiraBoardResponse.java
@@ -9,35 +9,9 @@ import java.util.List;
  * Created by rcharow on 10/2/16.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class JiraBoardResponse {
-    private Integer maxResults;
-    private Integer startAt;
-    private Boolean isLast;
+public class JiraBoardResponse extends JiraResponse{
     private List<JiraBoard> values;
 
-    public Integer getMaxResults() {
-        return maxResults;
-    }
-
-    public void setMaxResults(Integer maxResults) {
-        this.maxResults = maxResults;
-    }
-
-    public Integer getStartAt() {
-        return startAt;
-    }
-
-    public void setStartAt(Integer startAt) {
-        this.startAt = startAt;
-    }
-
-    public Boolean getLast() {
-        return isLast;
-    }
-
-    public void setLast(Boolean last) {
-        isLast = last;
-    }
 
     public List<JiraBoard> getValues() {
         return values;
