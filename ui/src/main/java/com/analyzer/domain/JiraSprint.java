5da8acbe0cb388691db3302e1a930f2fbfd33451
Rachel Charow <rachel.charow@gmail.com>
2016-10-08 17:16:35 -0400

    Add JiraSprint class

---
+++ ui/src/main/java/com/analyzer/domain/JiraSprint.java
@@ -0,0 +1,88 @@
+package com.analyzer.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Date;
+
+/**
+ * Created by rcharow on 10/8/16.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true )
+public class JiraSprint {
+    private Integer id;
+    private String self;
+    private String state;
+    private String name;
+    private Date startDate;
+    private Date endDate;
+    private Date completeDate;
+    private Integer originBoardId;
+
+    public enum sprintStates {
+        FUTURE, ACTIVE, CLOSED
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSelf() {
+        return self;
+    }
+
+    public void setSelf(String self) {
+        this.self = self;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCompleteDate() {
+        return completeDate;
+    }
+
+    public void setCompleteDate(Date completeDate) {
+        this.completeDate = completeDate;
+    }
+
+    public Integer getOriginBoardId() {
+        return originBoardId;
+    }
+
+    public void setOriginBoardId(Integer originBoardId) {
+        this.originBoardId = originBoardId;
+    }
+}
