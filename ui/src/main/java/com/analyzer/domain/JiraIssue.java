eaf0b00e387789c8ffd9c84832b1919836806d51
Rachel Charow <rachel.charow@gmail.com>
2016-10-09 15:06:31 -0400

    Add fields property to JiraIssue

--- ui/src/main/java/com/analyzer/domain/JiraIssue.java
+++ ui/src/main/java/com/analyzer/domain/JiraIssue.java
@@ -10,6 +10,7 @@ public class JiraIssue {
     private String id;
     private String self;
     private String key;
+    private JiraIssueFields fields;
 
     public String getId() {
         return id;
@@ -33,6 +34,14 @@ public class JiraIssue {
 
     public void setKey(String key) {
         this.key = key;
+    }
+
+    public JiraIssueFields getFields() {
+        return fields;
+    }
+
+    public void setFields(JiraIssueFields fields) {
+        this.fields = fields;
     }
 
     @Override
