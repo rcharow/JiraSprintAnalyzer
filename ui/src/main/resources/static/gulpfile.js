e21f595479323ac0577f8b4450cb210ec89b45ec
Rachel Charow <rachel.charow@gmail.com>
2016-12-11 14:48:52 -0500

    Recofigure gulp to work with new webpack setup

---
+++ ui/src/main/resources/static/gulpfile.js
@@ -0,0 +1,39 @@
+/* File: gulpfile.js */
+
+// grab our packages
+var gulp       = require('gulp'),
+    sass       = require('gulp-sass'),
+    webpack    = require('webpack-stream');
+
+var _output_dir = '../../../../target/classes/static/app/';
+
+// define the default task and add the watch task to it
+gulp.task('default', ['pack', 'build-css']);
+
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', ['watch-pack'], function() {
+  gulp.watch('./scss/**/*.scss', ['build-css']);
+});
+
+//pack ts to js with webpack
+gulp.task('pack', function () {
+    return pack('./webpack.config.js');
+});
+
+gulp.task('watch-pack', function () {
+    pack('./webpack.config.watch.js');
+});
+
+//compile sass files and move to generated resources
+gulp.task('build-css', function(){
+  return gulp.src(['./scss/**/*.scss'])
+     .pipe(sass().on('error', sass.logError))
+     .pipe(gulp.dest('../../../../target/generated-resources/static/css'));
+});
+
+function pack(config) {
+    return gulp.src('./app/vendor.js')
+        .pipe(webpack(require(config)))
+        .pipe(gulp.dest(_output_dir));
+}
