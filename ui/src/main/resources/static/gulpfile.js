73d32499467688f37a49395f5f9c6a24e0b9d981
rcharow <rachel.charow@portlandwebworks.com>
2016-12-22 10:12:59 -0500

    Refactor gulp sass process

--- ui/src/main/resources/static/gulpfile.js
+++ ui/src/main/resources/static/gulpfile.js
@@ -3,7 +3,8 @@
 // grab our packages
 var gulp       = require('gulp'),
     sass       = require('gulp-sass'),
-    webpack    = require('webpack-stream');
+    webpack    = require('webpack-stream'),
+    sourcemap  = require('gulp-sourcemaps');
 
 var _output_dir = '../../../../target/classes/static/app/';
 
@@ -13,7 +14,7 @@ gulp.task('default', ['pack', 'build-css']);
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', ['watch-pack'], function() {
-  gulp.watch('./scss/**/*.scss', ['build-css']);
+  gulp.watch('./scss/**/*.scss', ['sass-copy']);
 });
 
 //pack ts to js with webpack
@@ -25,11 +26,31 @@ gulp.task('watch-pack', function () {
     pack('./webpack.config.watch.js');
 });
 
-//compile sass files and move to generated resources
-gulp.task('build-css', function(){
-  return gulp.src(['./scss/**/*.scss'])
-     .pipe(sass().on('error', sass.logError))
-     .pipe(gulp.dest('./assets/css'));
+//compile sass files
+gulp.task('build-css', function () {
+    return gulp.src('./assets/scss/*.scss')
+        .pipe(sourcemap.init())
+        .pipe(sass({
+            verbose: true,
+            outputStyle: 'compressed',
+            includePaths: [
+                './node_modules/bootstrap/scss/',
+                require('bourbon').includePaths
+
+            ],
+            sourcemaps: true
+        }).on('error', function (err) {
+            console.error('\x07'); // so it doesn't just fail (literally) silently!
+            sass.logError.bind(this)(err);
+        }))
+        .pipe(sourcemap.write('./'))
+        .pipe(gulp.dest('./assets/css/'));
+});
+
+
+gulp.task('sass-copy', ['build-css'], function () {
+    return gulp.src('./assets/css/**/*')
+        .pipe(gulp.dest(_output_dir + '../assets/css'));
 });
 
 function pack(config) {
