82ee77d822d39d2714c1258060a1bd64a5178244
rcharow <rachel.charow@portlandwebworks.com>
2017-01-05 16:57:00 -0500

    Get the jira boards on init of the navbar component

--- ui/src/main/resources/static/app/navbar/navbar.component.ts
+++ ui/src/main/resources/static/app/navbar/navbar.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnalysisOptionsService, JiraBoard } from "../jira/jira.service";
 
 @Component({
     selector: 'navbar',
-    templateUrl: '/app/navbar/navbar.component.html'
+    templateUrl: '/app/navbar/navbar.component.html',
+    providers: [ AnalysisOptionsService ]
 })
 export class NavBarComponent{
+    analysisOptions: JiraBoard[];
+    constructor(private optionsService: AnalysisOptionsService) { }
 
+    ngOnInit() {
+        this.optionsService.getAnalysisOptions()
+            .subscribe(options => {
+                this.analysisOptions = options;
+            });
+    }
 }
