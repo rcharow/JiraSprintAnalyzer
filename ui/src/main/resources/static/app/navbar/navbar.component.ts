c8d4510c7461f198eb238e78fe030a4a7957ba67
rcharow <rachel.charow@portlandwebworks.com>
2017-01-05 17:43:31 -0500

    Remove the call to get jira boards from the navbar component

--- ui/src/main/resources/static/app/navbar/navbar.component.ts
+++ ui/src/main/resources/static/app/navbar/navbar.component.ts
@@ -1,19 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { AnalysisOptionsService, JiraBoard } from "../jira/jira.service";
+import { Component, OnInit, Input } from '@angular/core';
+import { JiraService, JiraBoard } from "../jira/jira.service";
 
 @Component({
     selector: 'navbar',
-    templateUrl: '/app/navbar/navbar.component.html',
-    providers: [ AnalysisOptionsService ]
+    templateUrl: '/app/navbar/navbar.component.html'
 })
 export class NavBarComponent{
-    analysisOptions: JiraBoard[];
-    constructor(private optionsService: AnalysisOptionsService) { }
+    @Input() boards: JiraBoard[];
+
+    constructor() { }
 
     ngOnInit() {
-        this.optionsService.getAnalysisOptions()
-            .subscribe(options => {
-                this.analysisOptions = options;
-            });
+
     }
 }
