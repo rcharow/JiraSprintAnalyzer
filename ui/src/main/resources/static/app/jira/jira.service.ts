f29390fdb9ace44739b85ca145da2b52c7987f3e
rcharow <rachel.charow@portlandwebworks.com>
2017-01-05 16:56:03 -0500

    Fix the analysis options service

--- ui/src/main/resources/static/app/jira/jira.service.ts
+++ ui/src/main/resources/static/app/jira/jira.service.ts
@@ -10,32 +10,14 @@ export class JiraBoard {
 
 }
 
-@Injectable
+@Injectable()
 export class AnalysisOptionsService {
-    constructor (private http: Http) {}
+    constructor (private http: Http) {
+        console.debug('AnalysisOptionsService initialized.')
+    }
 
     getAnalysisOptions (): Observable<JiraBoard[]> {
         return this.http.get('/api/jira/board')
-            .map(this.extractData)
-            .catch(this.handleError);
+            .map(res => res.json())
     }
-
-    private extractData(res: Response) {
-        let body = res.json();
-        return body.data || { };
-    }
-
-    private handleError (error: Response | any) {
-        // In a real world app, we might use a remote logging infrastructure
-        let errMsg: string;
-        if (error instanceof Response) {
-            const body = error.json() || '';
-            const err = body.error || JSON.stringify(body);
-            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-        } else {
-            errMsg = error.message ? error.message : error.toString();
-        }
-        console.error(errMsg);
-        return Observable.throw(errMsg);
-    }
-}
+};
