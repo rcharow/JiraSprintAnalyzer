35697becaab3873ba3d39e8b39890cbb6102681e
rcharow <rachel.charow@portlandwebworks.com>
2017-01-06 17:25:03 -0500

    Add output and board select handler to the navbar component to share the selected board with the home component

--- ui/src/main/resources/static/app/navbar/navbar.component.ts
+++ ui/src/main/resources/static/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { JiraService, JiraBoard } from "../jira/jira.service";
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'navbar',
@@ -7,10 +8,17 @@ import { JiraService, JiraBoard } from "../jira/jira.service";
 })
 export class NavBarComponent{
     @Input() boards: JiraBoard[];
+    @Output() selectBoard: EventEmitter<JiraBoard> = new EventEmitter<JiraBoard>();
+
+    private selectedBoard: JiraBoard;
 
     constructor() { }
 
     ngOnInit() {
 
+    }
+
+    onBoardSelect() {
+        this.selectBoard.emit(this.selectedBoard);
     }
 }
