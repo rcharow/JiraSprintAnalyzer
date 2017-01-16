4451c610d2266620c0a590fbbac703945276e00d
rcharow <rachel.charow@portlandwebworks.com>
2017-01-16 17:27:48 -0500

    Add vertical nav component

---
+++ ui/src/main/resources/static/app/vertical-nav/vertical-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'vertical-nav',
+    templateUrl: '/app/vertical-nav/vertical-nav.component.html'
+})
+
+export class VerticalNavComponent{
+
+}
