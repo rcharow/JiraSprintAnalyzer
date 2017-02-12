fa5dfba8843f9b92b2f4c303e14703bb619e59bc
rcharow <rachel.charow@portlandwebworks.com>
2017-02-12 17:16:53 -0500

    Remove fetching the list of jira boards from the home compoent; this has moved to the dashboards options component

--- ui/src/main/resources/static/app/home.component.ts
+++ ui/src/main/resources/static/app/home.component.ts
@@ -1,28 +1,12 @@
 import { Component } from '@angular/core';
-import { JiraService, JiraBoard } from "./jira/jira.service";
 
 @Component({
     selector: 'home',
     templateUrl: '/app/home.component.html',
-    providers: [ JiraService ]
 })
 export class HomeComponent{
-    boards: JiraBoard[] = [];
-    selectedBoard: JiraBoard = new JiraBoard();
 
-    constructor(private jiraService: JiraService) {
+    constructor() {
         console.debug('Home component loaded!');
     };
-
-    ngOnInit() {
-        this.jiraService.getBoards()
-            .subscribe(boards => {
-                this.boards = boards;
-            });
-    }
-
-    onBoardSelect(board: JiraBoard) {
-        this.selectedBoard = board;
-    }
-
 }
