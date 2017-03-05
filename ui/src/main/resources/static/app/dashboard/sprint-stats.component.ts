16a92e294402647566e9994538c0d4e2f667fd7f
rcharow <rachel.charow@portlandwebworks.com>
2017-03-05 14:12:51 -0500

    Add sprint stats data to the sprint stats component

--- ui/src/main/resources/static/app/dashboard/sprint-stats.component.ts
+++ ui/src/main/resources/static/app/dashboard/sprint-stats.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {JiraSprintStats} from "../jira/jira.model";
 
 @Component({
-    selector: 'sprint-stats',
-    templateUrl: '/app/dashboard/single-sprint-stats.component.html'
+  selector: 'sprint-stats',
+  templateUrl: '/app/dashboard/sprint-stats.component.html'
 })
 
-export class SprintStatsComponent {
+export class SprintStatsComponent implements OnInit {
+  stats: JiraSprintStats[];
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit(){
+    this.route.data.subscribe((data:{ stats: JiraSprintStats[] }) => {
+      this.stats = data.stats;
+    });
+  }
 
 }
