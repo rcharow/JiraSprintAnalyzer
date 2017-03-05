55a1ce9cdd49d2f73015444e861d6df3296d9e0d
rcharow <rachel.charow@portlandwebworks.com>
2017-03-05 14:13:47 -0500

    Add jira sprint stats to jira model

--- ui/src/main/resources/static/app/jira/jira.model.ts
+++ ui/src/main/resources/static/app/jira/jira.model.ts
@@ -16,3 +16,13 @@ export class JiraSprint {
   public completeDate: Date;
   public originBoardId: String;
 }
+
+
+export class JiraSprintStats {
+  public id: String;
+  public name: String;
+  public totalIssues: Number;
+  public totalPoints: Number;
+  public totalTimeSeconds: Number;
+  public totalCost: Number;
+}
