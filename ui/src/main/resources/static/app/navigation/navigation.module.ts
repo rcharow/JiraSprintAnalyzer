bb8559427e01cb63fd056f5e22eb2f968294dbe3
rcharow <rachel.charow@portlandwebworks.com>
2017-03-05 14:15:31 -0500

    Add the router module to the navigation module

--- ui/src/main/resources/static/app/navigation/navigation.module.ts
+++ ui/src/main/resources/static/app/navigation/navigation.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { JiraService } from '../jira/jira.service';
 import { NavBarComponent } from './navbar.component';
 import { PillNavComponent } from './pill-nav.component';
 import { VerticalNavComponent } from './vertical-nav.component';
+import { DashboardService } from "../dashboard/dashboard.service";
 
 @NgModule({
-    imports: [ SharedModule ],
+    imports: [
+      SharedModule,
+      RouterModule
+    ],
     declarations: [
        NavBarComponent,
        PillNavComponent,
        VerticalNavComponent
     ],
     exports: [
-        NavBarComponent,
-        PillNavComponent,
-        VerticalNavComponent
+      NavBarComponent,
+      PillNavComponent,
+      VerticalNavComponent
     ],
-    providers: [JiraService]
+    providers: [
+      JiraService
+    ]
 })
 export class NavigationModule { }
