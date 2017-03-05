16d59b0c2899a97533a62706396aa9e2c63add0a
rcharow <rachel.charow@portlandwebworks.com>
2017-03-05 14:11:44 -0500

    Add the sprints stats component and the dashboard service to the dashboard module

--- ui/src/main/resources/static/app/dashboard/dashboard.module.ts
+++ ui/src/main/resources/static/app/dashboard/dashboard.module.ts
@@ -1,29 +1,35 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { DashboardRoutingModule } from "./dashboard-routing.module";
 import { JiraService } from "../jira/jira.service";
 import { DashboardComponent } from "./dashboard.component";
-import { SingleSprintStatsComponent } from './single-sprint-stats.component';
+import { SprintStatsComponent } from './sprint-stats.component';
 import { NavigationModule } from "../navigation/navigation.module";
 import { DashboardOptionsComponent } from "./dashboard-options.component";
 import { SharedModule } from "../shared/shared.module";
+import { DashboardService } from "./dashboard.service";
 
 @NgModule({
   imports: [
+    RouterModule,
     SharedModule,
     DashboardRoutingModule,
     NavigationModule
   ],
   declarations: [
     DashboardComponent,
-    SingleSprintStatsComponent,
+    SprintStatsComponent,
     DashboardOptionsComponent
   ],
   exports: [
     DashboardComponent,
-    SingleSprintStatsComponent,
+    SprintStatsComponent,
     DashboardOptionsComponent
   ],
-  providers: [JiraService]
+  providers: [
+    JiraService,
+    DashboardService
+  ]
 })
 export class DashboardModule {
 }
