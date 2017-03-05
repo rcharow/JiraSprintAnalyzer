41449700bbfc8ebc8da25023372d7cd0c58e69f9
rcharow <rachel.charow@portlandwebworks.com>
2017-03-05 14:13:26 -0500

    Update stats resolve service with http call for stats data

--- ui/src/main/resources/static/app/dashboard/stats-resolve.service.ts
+++ ui/src/main/resources/static/app/dashboard/stats-resolve.service.ts
@@ -4,14 +4,17 @@ import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { JiraService } from '../jira/jira.service';
 import { JiraSprint } from '../jira/jira.model';
+import {JiraSprintStats} from "../jira/jira.model";
 
 @Injectable()
-export class DashboardResolveService implements Resolve<Observable<JiraSprint[]>> {
+export class StatsResolveService implements Resolve<Observable<JiraSprintStats[]>> {
 
     constructor(private service: JiraService){}
 
-    resolve(route: ActivatedRouteSnapshot): Observable<JiraSprint[]> {
+    resolve(route: ActivatedRouteSnapshot): Observable<JiraSprintStats[]> {
         let boardId: String = route.params['boardId'];
-        return this.service.getSprintsByBoardId(boardId);
+        let startSprintCompleteDate: Date = route.params['start'];
+        let endSprintCompleteDate: Date = route.params['end'];
+        return this.service.getSprintStats(boardId, startSprintCompleteDate, endSprintCompleteDate);
     }
 }
