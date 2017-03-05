5e22819a034b32fcbb9695ab1d38f6de96ebb3fe
rcharow <rachel.charow@portlandwebworks.com>
2017-03-05 14:07:42 -0500

    Rename single sprint stats component to sprint stats

--- ui/src/main/resources/static/app/dashboard/sprint-stats.component.ts
+++ ui/src/main/resources/static/app/dashboard/sprint-stats.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'single-sprint-stats',
+    selector: 'sprint-stats',
     templateUrl: '/app/dashboard/single-sprint-stats.component.html'
 })
 
-export class SingleSprintStatsComponent {
+export class SprintStatsComponent {
 
 }
