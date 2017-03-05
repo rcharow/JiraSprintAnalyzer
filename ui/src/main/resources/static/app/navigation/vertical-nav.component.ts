58b65ecf4ef16a58d18f14924cd7554b5c11dc2e
rcharow <rachel.charow@portlandwebworks.com>
2017-03-05 14:17:00 -0500

    Subscribe to dashboard service data on init

--- ui/src/main/resources/static/app/navigation/vertical-nav.component.ts
+++ ui/src/main/resources/static/app/navigation/vertical-nav.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {JiraBoard} from "../jira/jira.model";
+import {DashboardService} from "../dashboard/dashboard.service";
+import {JiraSprint} from "../jira/jira.model";
 
 @Component({
     selector: 'vertical-nav',
@@ -6,5 +10,28 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 
 export class VerticalNavComponent{
+    currentBoard:JiraBoard;
+    startSprint:JiraSprint;
+    endSprint:JiraSprint;
+    isMultiSprint:boolean;
 
+    constructor(private dashboardService:DashboardService){}
+
+    ngOnInit(){
+        this.dashboardService.currentBoard.subscribe((board:JiraBoard) => {
+            this.currentBoard = board;
+        });
+
+        this.dashboardService.startSprint.subscribe((sprint:JiraSprint) => {
+            this.startSprint = sprint;
+        });
+
+        this.dashboardService.endSprint.subscribe((sprint:JiraSprint) => {
+            this.endSprint = sprint;
+        });
+
+        this.dashboardService.isMultiSprint.subscribe((isMultiSprint:boolean) => {
+            this.isMultiSprint = isMultiSprint;
+        });
+    }
 }
