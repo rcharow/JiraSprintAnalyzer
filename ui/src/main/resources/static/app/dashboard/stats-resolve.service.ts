ff242d88f4288ddc751b083724dde268d9d55b3d
rcharow <rachel.charow@portlandwebworks.com>
2017-03-16 17:42:34 -0400

    Update resolve to just get the summary for one sprint

--- ui/src/main/resources/static/app/dashboard/stats-resolve.service.ts
+++ ui/src/main/resources/static/app/dashboard/stats-resolve.service.ts
@@ -4,17 +4,18 @@ import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { JiraService } from '../jira/jira.service';
 import { JiraSprint } from '../jira/jira.model';
-import {JiraSprintStats} from "../jira/jira.model";
+import {JiraSprintSummary} from "../jira/jira.model";
 
 @Injectable()
-export class StatsResolveService implements Resolve<Observable<JiraSprintStats[]>> {
+export class StatsResolveService implements Resolve<Observable<JiraSprintSummary[]>> {
 
     constructor(private service: JiraService){}
 
-    resolve(route: ActivatedRouteSnapshot): Observable<JiraSprintStats[]> {
-        let boardId: String = route.params['boardId'];
+    resolve(route: ActivatedRouteSnapshot): Observable<JiraSprintSummary> {
+        let sprintId: String = route.params['sprintId'];
         let startSprintCompleteDate: Date = route.params['start'];
         let endSprintCompleteDate: Date = route.params['end'];
-        return this.service.getSprintStats(boardId, startSprintCompleteDate, endSprintCompleteDate);
+
+        return this.service.getSingleSprintSummary(sprintId);
     }
 }
