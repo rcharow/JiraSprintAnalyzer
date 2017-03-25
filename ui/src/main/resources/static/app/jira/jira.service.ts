3d1c8850517b85ddcc60ed6892c19d02bd234526
rcharow <rachel.charow@portlandwebworks.com>
2017-03-25 12:50:41 -0400

    Formatting

--- ui/src/main/resources/static/app/jira/jira.service.ts
+++ ui/src/main/resources/static/app/jira/jira.service.ts
@@ -6,28 +6,26 @@ import { JiraSprintSummary } from "./jira.model";
 
 @Injectable()
 export class JiraService {
-    constructor (private http: Http) {
-        console.debug('JiraService initialized.')
-    }
+  constructor(private http:Http) {}
 
-    getBoards (): Observable<JiraBoard[]> {
-        return this.http.get('/api/jira/board')
-            .map(res => res.json())
-    }
+  getBoards():Observable<JiraBoard[]> {
+    return this.http.get('/api/jira/board')
+      .map(res => res.json())
+  }
 
-    getScrumBoards (): Observable<JiraBoard[]> {
-        return this.http.get('/api/jira/board/type/scrum')
-          .map(res => res.json())
-    }
+  getScrumBoards():Observable<JiraBoard[]> {
+    return this.http.get('/api/jira/board/type/scrum')
+      .map(res => res.json())
+  }
 
-    getClosedSprintsByBoardId (boardId: String): Observable<JiraSprint[]> {
-        return this.http.get('/api/jira/board/' + boardId + '/sprints/closed')
-            .map(res => res.json());
-    }
+  getClosedSprintsByBoardId(boardId:String):Observable<JiraSprint[]> {
+    return this.http.get('/api/jira/board/' + boardId + '/sprints/closed')
+      .map(res => res.json());
+  }
 
-    getSingleSprintSummary (sprintId: String): Observable<JiraSprintSummary> {
-        return this.http.get('/api/analysis/summary/' + sprintId)
-          .map(res => res.json());
-    }
+  getSingleSprintSummary(sprintId:String):Observable<JiraSprintSummary> {
+    return this.http.get('/api/analysis/summary/' + sprintId)
+      .map(res => res.json());
+  }
 
 }
