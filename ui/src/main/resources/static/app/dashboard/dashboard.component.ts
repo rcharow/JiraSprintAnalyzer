2595b980f6907f1b99a4f57e0943cf191a227f76
rcharow <rachel.charow@portlandwebworks.com>
2017-03-25 12:53:26 -0400

    Listen for routing events in the dashboard component, and change the loading status of the dashboard component based on them

--- ui/src/main/resources/static/app/dashboard/dashboard.component.ts
+++ ui/src/main/resources/static/app/dashboard/dashboard.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { DashboardService } from './dashboard.service';
+import { split } from 'lodash';
 
 @Component({
-    selector: 'dashboard',
-    templateUrl: '/app/dashboard/dashboard.component.html'
+  selector: 'dashboard',
+  templateUrl: '/app/dashboard/dashboard.component.html'
 })
 
 export class DashboardComponent {
+
+  private loading:boolean = false;
+
+  constructor(private router:Router, private dashboardService:DashboardService) {
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (split(event.url, '/')[1] === 'dashboard') {
+        switch (event.constructor) {
+          case NavigationStart:
+            this.loading = true;
+            this.dashboardService.setDashboardLoading(true);
+            break;
+          case NavigationEnd:
+            this.loading = false;
+            this.dashboardService.setDashboardLoading(false);
+            break;
+        }
+      }
+    });
+
+    this.dashboardService.isDashboardLoading.subscribe((loading:boolean) => {
+      this.loading = loading;
+    });
+  }
 
 }
