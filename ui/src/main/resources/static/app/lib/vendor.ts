3711c226029fe29039dcd6a8d569909d07038436
rcharow <rachel.charow@portlandwebworks.com>
2017-05-29 11:34:44 -0400

    Updating dependencies

--- ui/src/main/resources/static/app/lib/vendor.ts
+++ ui/src/main/resources/static/app/lib/vendor.ts
@@ -16,7 +16,6 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/publishReplay';
-import 'rxjs/add/operator/cache';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/delay';
 
