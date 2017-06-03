7b23b14b96de18a667349c27c1f9eed42842808f
rcharow <rachel.charow@portlandwebworks.com>
2017-06-03 14:28:49 -0400

    Clean up unused parts of dashboard component

--- ui/src/main/resources/static/app/dashboard/dashboard.component.ts
+++ ui/src/main/resources/static/app/dashboard/dashboard.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { Component } from '@angular/core';
 import { DashboardService } from './dashboard.service';
-import { split } from 'lodash';
 
 @Component({
   selector: 'dashboard',
@@ -12,26 +10,10 @@ export class DashboardComponent {
 
   private loading:boolean = false;
 
-  constructor(private router:Router, private dashboardService:DashboardService) {
+  constructor(private dashboardService:DashboardService) {
   }
 
   ngOnInit() {
-    //TODO: Should this happen in a higher-level component?
-    //this.router.events.subscribe((event) => {
-    //  if (split(event.url, '/')[1] === 'dashboard') {
-    //    switch (event.constructor) {
-    //      case NavigationStart:
-    //        this.loading = true;
-    //        this.dashboardService.setDashboardLoading(true);
-    //        break;
-    //      case NavigationEnd:
-    //        this.loading = false;
-    //        this.dashboardService.setDashboardLoading(false);
-    //        break;
-    //    }
-    //  }
-    //});
-
     this.dashboardService.isDashboardLoading.subscribe((loading:boolean) => {
       this.loading = loading;
     });
