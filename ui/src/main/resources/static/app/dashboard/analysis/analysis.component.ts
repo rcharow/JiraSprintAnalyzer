f730d27dd0a2b6d2d1d931440f1aa97b435e3e90
rcharow <rachel.charow@portlandwebworks.com>
2017-06-14 09:21:53 -0400

    Refactor analysis component to check for changes in the query parameters before calling data services

--- ui/src/main/resources/static/app/dashboard/analysis/analysis.component.ts
+++ ui/src/main/resources/static/app/dashboard/analysis/analysis.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { DashboardService } from "../../dashboard/dashboard.service";
-import { JiraService } from "../../jira/jira.service";
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {DashboardService} from "../../dashboard/dashboard.service";
+import {JiraService} from "../../jira/jira.service";
+import {difference} from "lodash";
 
 
 @Component({
@@ -9,15 +10,27 @@ import { JiraService } from "../../jira/jira.service";
   templateUrl: '/app/dashboard/analysis/analysis.component.html'
 })
 
-export class AnalysisComponent implements OnInit{
+export class AnalysisComponent implements OnInit {
 
-  constructor(private route:ActivatedRoute, private dashboardService:DashboardService, private jiraService:JiraService) {}
+  private currentSprints: string[] = [];
 
-  ngOnInit(){
-      this.route.queryParams.subscribe((queryParams:any) => {
-        let sprints = queryParams['sprints'];
+  constructor(private route: ActivatedRoute, private dashboardService: DashboardService, private jiraService: JiraService) {
+  }
+
+  ngOnInit() {
+
+    this.dashboardService.currentSprints.subscribe((sprints: string[]) => {
+      this.currentSprints = sprints;
+    });
+
+    this.route.queryParams.subscribe((queryParams: any) => {
+      let sprints = typeof queryParams['sprints'] === 'string' ? [queryParams['sprints']] : queryParams['sprints'];
+      
+      //TODO: Is there a better way to do this? Can angular handle it?
+      if(difference(sprints, this.currentSprints).length || difference(this.currentSprints, sprints).length) {
         this.dashboardService.setCurrentSprints(sprints);
         this.jiraService.setCurrentSummary(sprints);
-      });
+      }
+    });
   }
 }
