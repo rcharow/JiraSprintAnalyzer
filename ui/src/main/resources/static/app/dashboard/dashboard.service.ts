9f693021af1d4f536f46f22ada5ba22e3900a707
rcharow <rachel.charow@portlandwebworks.com>
2017-06-14 09:22:12 -0400

    Reformatting

--- ui/src/main/resources/static/app/dashboard/dashboard.service.ts
+++ ui/src/main/resources/static/app/dashboard/dashboard.service.ts
@@ -1,28 +1,30 @@
-import { Injectable } from '@angular/core';
-import { JiraBoard } from "../jira/jira.model";
-import { JiraSprint } from "../jira/jira.model";
-import { BehaviorSubject } from "rxjs/BehaviorSubject";
-import { Observable } from "rxjs/Observable";
+import {Injectable} from '@angular/core';
+import {JiraBoard} from "../jira/jira.model";
+import {JiraSprint} from "../jira/jira.model";
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
+import {Observable} from "rxjs/Observable";
+import {difference} from "lodash";
 
 @Injectable()
 export class DashboardService {
   private _currentBoard: BehaviorSubject<JiraBoard> = new BehaviorSubject(null);
-  private _currentSprints: BehaviorSubject<Array<string>> = new BehaviorSubject(null);
+  private _currentSprints: BehaviorSubject<Array<string>> = new BehaviorSubject([]);
   private _isDashboardLoading: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   public currentBoard: Observable<JiraBoard> = this._currentBoard.asObservable();
   public currentSprints: Observable<Array<string>> = this._currentSprints.asObservable();
   public isDashboardLoading: Observable<boolean> = this._isDashboardLoading.asObservable();
 
-  setCurrentBoard(board:JiraBoard) {
+  setCurrentBoard(board: JiraBoard) {
     this._currentBoard.next(board);
   }
 
-  setCurrentSprints(sprintIds:Array<string>){
+  setCurrentSprints(sprintIds: Array<string>) {
+    //TODO: Do I need to convert to array here?
     this._currentSprints.next(sprintIds);
   }
 
-  setDashboardLoading(loading:boolean) {
+  setDashboardLoading(loading: boolean) {
     this._isDashboardLoading.next(loading);
   }
 
