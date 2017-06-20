b683b5554274ce49f88d5e77063c106bca5a467f
rcharow <rachel.charow@portlandwebworks.com>
2017-06-20 16:43:42 -0400

    Add chart to money analysis component

--- ui/src/main/resources/static/app/dashboard/analysis/money/money.component.ts
+++ ui/src/main/resources/static/app/dashboard/analysis/money/money.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {JiraBoard, JiraSprintPointAnalysis} from "../../../jira/jira.model";
+import {JiraService} from "../../../jira/jira.service";
+import {forIn, each} from "lodash";
 
 
 @Component({
@@ -7,5 +10,84 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MoneyComponent {
+  board: JiraBoard[] = null;
+  analysis: JiraSprintPointAnalysis[];
+  loading: Boolean;
+  type: string = 'line';
+  chartData:object = {};
+  chartColors: string[] = ['#F19F4D', '#4484CE', '#344D68', '#9FD356', '#034748', '#3C1518', '#A14EBF', '#A63D40', '#EE6C4D'];
+  chartOptions:object = {
+    responsive: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          callback(value:number){
+            return '$' + value.toFixed(2);
+          }
+        }
+      }]
+    }
+  };
 
+  constructor(private jiraService: JiraService) {
+  }
+
+  ngOnInit() {
+    this.loading = true;
+
+    this.jiraService.currentPointAnalysis.subscribe((analysis: JiraSprintPointAnalysis[]) => {
+      this.analysis = analysis;
+      this.loading = !analysis;
+      this.formatData(analysis);
+    });
+  }
+
+  formatData(data: JiraSprintPointAnalysis[]) {
+    let pointValues = {};
+    let labels: string[] = [];
+    each(data, sprint => {
+      labels.push(sprint.sprintName);
+      each(sprint.pointAnalysis, analysis => {
+        if (!pointValues[analysis.pointValue]) {
+          pointValues[analysis.pointValue] = [{x: sprint.sprintName, y: analysis.avgDollarsSpent}];
+        } else {
+          pointValues[analysis.pointValue].push({x: sprint.sprintName, y: analysis.avgDollarsSpent});
+        }
+      });
+    });
+
+    let datasets: Object[] = [];
+    let idx: number = 0;
+    forIn(pointValues, (value, key) => {
+      let color = idx < this.chartColors.length ? this.chartColors[idx] : null;
+      datasets.push({
+        label: key,
+        data: value,
+        fill: false,
+        borderColor: color,
+        backgroundColor: color
+      });
+      idx++;
+    });
+
+    this.chartData = {
+      labels: labels,
+      datasets: datasets
+
+    }
+  }
 }
+
+// let datasets = map(data, sprint => {
+//   let data = map(sprint.pointAnaylis, analysis => {
+//     return {
+//       x: analysis.pointValue,
+//       y: analysis.avgDollars
+//     }
+//   });
+//
+//   return {
+//     label: sprint.sprintName,
+//     data: data
+//   };
+// });
