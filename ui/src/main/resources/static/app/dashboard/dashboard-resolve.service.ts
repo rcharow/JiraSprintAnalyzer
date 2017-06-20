b3c965eec68b6ccf5e4adc6ab0ef8a2c86073240
rcharow <rachel.charow@portlandwebworks.com>
2017-06-20 16:41:18 -0400

    Rename and update resolve service

---
+++ ui/src/main/resources/static/app/dashboard/dashboard-resolve.service.ts
@@ -0,0 +1,15 @@
+
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { DashboardService } from './dashboard.service';
+
+@Injectable()
+export class DashboardResolveService implements Resolve<boolean> {
+
+    constructor(private service: DashboardService){}
+
+    resolve(route: ActivatedRouteSnapshot): boolean {
+        this.service.setCurrenView(route.url[0].path);
+        return true;
+    }
+}
