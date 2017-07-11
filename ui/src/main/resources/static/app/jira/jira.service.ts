6c94b3a3e546fc92f3f372dcaaf78ec402d7c5ec
rcharow <rachel.charow@portlandwebworks.com>
2017-07-11 11:36:42 -0400

    Add minimal error handling to jira service so the loading indicator doesn't show if loading data has failed

--- ui/src/main/resources/static/app/jira/jira.service.ts
+++ ui/src/main/resources/static/app/jira/jira.service.ts
@@ -1,117 +1,134 @@
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
-import { BehaviorSubject } from "rxjs/BehaviorSubject";
-import { JiraBoard, JiraSprint } from './jira.model';
-import { JiraSprintSummary, JiraWorklogSummary, JiraSprintPointAnalysis } from "./jira.model";
-import { each, join } from "lodash";
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
+import {JiraBoard, JiraSprint} from './jira.model';
+import {JiraSprintSummary, JiraWorklogSummary, JiraSprintPointAnalysis} from "./jira.model";
+import {each, join} from "lodash";
 
 @Injectable()
 export class JiraService {
-  private _currentSummary:BehaviorSubject<JiraSprintSummary[]> = new BehaviorSubject(null);
-  private _currentWorklogSummary:BehaviorSubject<JiraWorklogSummary[]> = new BehaviorSubject(null);
-  private _currentPointAnalysis:BehaviorSubject<JiraSprintPointAnalysis[]> = new BehaviorSubject(null);
-  private _currentSummaryLoading:BehaviorSubject<boolean> = new BehaviorSubject(false);
-  private _currentWorklogSummaryLoading:BehaviorSubject<boolean> = new BehaviorSubject(false);
-  private _currentPointAnalysisLoading:BehaviorSubject<boolean> = new BehaviorSubject(false);
+  private _currentSummary: BehaviorSubject<JiraSprintSummary[]> = new BehaviorSubject(null);
+  private _currentWorklogSummary: BehaviorSubject<JiraWorklogSummary[]> = new BehaviorSubject(null);
+  private _currentPointAnalysis: BehaviorSubject<JiraSprintPointAnalysis[]> = new BehaviorSubject(null);
+  private _currentSummaryLoading: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  private _currentWorklogSummaryLoading: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  private _currentPointAnalysisLoading: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
-  public currentSummary:Observable<JiraSprintSummary[]> = this._currentSummary.asObservable();
-  public currentWorklogSummary:Observable<JiraWorklogSummary[]> = this._currentWorklogSummary.asObservable();
-  public currentPointAnalysis:Observable<JiraSprintPointAnalysis[]> = this._currentPointAnalysis.asObservable();
-  public currentSummaryLoading:Observable<boolean> = this._currentSummaryLoading.asObservable();
-  public currentWorklogSummaryLoading:Observable<boolean> = this._currentWorklogSummaryLoading.asObservable();
-  public currentPointAnalysisLoading:Observable<boolean> = this._currentPointAnalysisLoading.asObservable();
+  public currentSummary: Observable<JiraSprintSummary[]> = this._currentSummary.asObservable();
+  public currentWorklogSummary: Observable<JiraWorklogSummary[]> = this._currentWorklogSummary.asObservable();
+  public currentPointAnalysis: Observable<JiraSprintPointAnalysis[]> = this._currentPointAnalysis.asObservable();
+  public currentSummaryLoading: Observable<boolean> = this._currentSummaryLoading.asObservable();
+  public currentWorklogSummaryLoading: Observable<boolean> = this._currentWorklogSummaryLoading.asObservable();
+  public currentPointAnalysisLoading: Observable<boolean> = this._currentPointAnalysisLoading.asObservable();
 
-  constructor(private http:Http) {
+  constructor(private http: Http) {
   }
 
 
-  getBoards():Observable<JiraBoard[]> {
+  getBoards(): Observable<JiraBoard[]> {
     return this.http.get('/api/jira/board')
       .map(res => res.json())
   }
 
-  getScrumBoards():Observable<JiraBoard[]> {
+  getScrumBoards(): Observable<JiraBoard[]> {
     return this.http.get('/api/jira/board/type/scrum')
       .map(res => res.json())
   }
 
-  getClosedSprintsByBoardId(boardId:String):Observable<JiraSprint[]> {
+  getClosedSprintsByBoardId(boardId: String): Observable<JiraSprint[]> {
     return this.http.get('/api/jira/board/' + boardId + '/sprints/closed')
       .map(res => res.json());
   }
 
-  getSingleSprintSummary(boardId:string, sprintId:string):Observable<JiraSprintSummary> {
+  getSingleSprintSummary(boardId: string, sprintId: string): Observable<JiraSprintSummary> {
     return this.http.get('/api/analysis/summary/' + boardId + '/' + sprintId)
       .map(res => res.json());
   }
 
-  setCurrentSummary(boardId:string, sprintIds:string[]) {
-      sprintIds = typeof sprintIds === 'string' ? [sprintIds] : sprintIds;
-      this._currentSummary.next(null);
-      this._currentSummaryLoading.next(true);
+  setCurrentSummary(boardId: string, sprintIds: string[]) {
+    sprintIds = typeof sprintIds === 'string' ? [sprintIds] : sprintIds;
+    this._currentSummary.next(null);
+    this._currentSummaryLoading.next(true);
 
-      let requests:Observable<JiraSprintSummary>[] = [];
-      each(sprintIds, (sprintId) => {
-        requests.push(this.http.get('/api/analysis/summary/worklogs/' + boardId + '/' + sprintId).map(res => res.json()));
-      });
+    let requests: Observable<JiraSprintSummary>[] = [];
+    each(sprintIds, (sprintId) => {
+      requests.push(this.http.get('/api/analysis/summary/worklogs/' + boardId + '/' + sprintId).map(res => res.json()));
+    });
 
-      Observable.forkJoin(requests).subscribe((results:JiraSprintSummary[]) => {
-        results.sort((a:JiraSprintSummary, b: JiraSprintSummary) => {
-          if(a.completeDate > b.completeDate) return 1;
-          if(a.completeDate < b.completeDate) return -1;
+    Observable.forkJoin(requests).subscribe((results: JiraSprintSummary[]) => {
+        results.sort((a: JiraSprintSummary, b: JiraSprintSummary) => {
+          if (a.completeDate > b.completeDate) return 1;
+          if (a.completeDate < b.completeDate) return -1;
           return 0;
         });
         this._currentSummary.next(results);
         this._currentSummaryLoading.next(false);
-      });
+      },
+      error => {
+        this._currentSummaryLoading.next(false);
+        console.log('Error getting sprint summary data.');
+      }
+    );
   }
 
-  setCurrentWorklogSummary(boardId:string, sprintIds:string[]) {
-    if(!this._currentWorklogSummary.getValue()){
+  setCurrentWorklogSummary(boardId: string, sprintIds: string[]) {
+    if (!this._currentWorklogSummary.getValue()) {
       this._currentWorklogSummary.next(null);
       this._currentWorklogSummaryLoading.next(true);
 
-      this.http.get('/api/analysis/worklogs/' + boardId + '/' + join(sprintIds,','))
+      this.http.get('/api/analysis/worklogs/' + boardId + '/' + join(sprintIds, ','))
         .map(res => res.json())
-        .subscribe((summary:JiraWorklogSummary[]) => {
-        this._currentWorklogSummary.next(summary);
-        this._currentWorklogSummaryLoading.next(false);
-      });
+        .subscribe((summary: JiraWorklogSummary[]) => {
+          this._currentWorklogSummary.next(summary);
+          this._currentWorklogSummaryLoading.next(false);
+        },
+          error => {
+            this._currentWorklogSummaryLoading.next(false);
+            console.log('Error getting sprint worklog summary data.');
+          });
     }
   }
 
-  setCurrentPointAnalysisByBoard(boardId:string) {
+  setCurrentPointAnalysisByBoard(boardId: string) {
     this._currentPointAnalysis.next(null);
     this._currentPointAnalysisLoading.next(true);
 
     this.http.get('/api/analysis/point/' + boardId)
       .map(res => res.json())
-      .subscribe((data:JiraSprintPointAnalysis[]) => {
+      .subscribe((data: JiraSprintPointAnalysis[]) => {
         this._currentPointAnalysis.next(data);
         this._currentPointAnalysisLoading.next(false);
-      });
+      },
+        error => {
+          this._currentPointAnalysisLoading.next(false);
+          console.log('Error getting point analysis data by board.');
+        });
   }
 
-  setCurrentPointAnalysisBySprints(boardId:string, sprintIds:string[]) {
+  setCurrentPointAnalysisBySprints(boardId: string, sprintIds: string[]) {
     sprintIds = typeof sprintIds === 'string' ? [sprintIds] : sprintIds;
     this._currentPointAnalysis.next(null);
     this._currentPointAnalysisLoading.next(true);
 
-    let requests:Observable<JiraSprintPointAnalysis>[] = [];
+    let requests: Observable<JiraSprintPointAnalysis>[] = [];
     each(sprintIds, (sprintId) => {
       requests.push(this.http.get('/api/analysis/point/' + boardId + '/sprint/' + sprintId).map(res => res.json()));
     });
 
-    Observable.forkJoin(requests).subscribe((results:JiraSprintPointAnalysis[]) => {
-      results.sort((a:JiraSprintPointAnalysis, b: JiraSprintPointAnalysis) => {
-        if(a.endDate > b.endDate) return 1;
-        if(a.endDate < b.endDate) return -1;
+    Observable.forkJoin(requests).subscribe((results: JiraSprintPointAnalysis[]) => {
+      results.sort((a: JiraSprintPointAnalysis, b: JiraSprintPointAnalysis) => {
+        if (a.endDate > b.endDate) return 1;
+        if (a.endDate < b.endDate) return -1;
         return 0;
       });
       this._currentPointAnalysis.next(results);
       this._currentPointAnalysisLoading.next(false);
-    });
+    },
+      error => {
+        this._currentPointAnalysisLoading.next(false);
+        console.log('Error getting point analysis data by sprints.');
+      });
   }
 
 }
