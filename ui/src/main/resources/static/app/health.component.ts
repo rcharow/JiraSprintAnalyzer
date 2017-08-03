f43241b236e7a3d21b21327521616ef78a1ad3bd
rcharow <rachel.charow@portlandwebworks.com>
2017-08-03 16:22:18 -0400

    Add health page for lyle

---
+++ ui/src/main/resources/static/app/health.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'health',
+  template: '<div class="container"><div class="row"></div><div class="text-center mt-5"><i class="fa fa-5x fa-medkit" aria-hidden="true"></i></div><div class="text-center mt-3">Healthy!</div></div>'
+})
+export class HealthComponent{}
