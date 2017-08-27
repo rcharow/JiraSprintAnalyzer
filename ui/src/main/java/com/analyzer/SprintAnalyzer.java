68e2da1f641cfbe7a35a464e67a8d7b0d5ed74d5
rcharow <rachel.charow@portlandwebworks.com>
2017-08-27 14:26:25 -0400

    Enable scheduling in application

--- ui/src/main/java/com/analyzer/SprintAnalyzer.java
+++ ui/src/main/java/com/analyzer/SprintAnalyzer.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,6 +20,7 @@ import javax.ws.rs.ApplicationPath;
 
 @EnableWebSecurity
 @EnableTransactionManagement
+@EnableScheduling
 @SpringBootApplication
 @ApplicationPath("/api")
 public class SprintAnalyzer extends ResourceConfig {
